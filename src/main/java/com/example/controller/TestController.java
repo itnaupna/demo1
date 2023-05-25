1e0011d85916b8190dafdd21ec080d137ae10e42
itnaupna <itnaupna@gmail.com>
2023-05-25 18:17:45 +0900

    Initial commit

---
+++ src/main/java/com/example/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/eong")
+    public String eong(){
+        return "eoooong";
+    }
+}
